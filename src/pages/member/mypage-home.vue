0d61c0592daa7aa23a84e3ff11517a1bd0f46734
LEE <evisahah@gmail.com>
2023-03-25 11:27:21 +0900

    마이페이지

--- src/pages/member/mypage-home.vue
+++ src/pages/member/mypage-home.vue
@@ -15,15 +15,36 @@
                                         <div class="main-button" style="margin-bottom: 20px;">
                                             <a href="/logout">로그아웃</a>
                                         </div>
-                                        <h4>{{ name }}님, 환영합니다</h4>
+                                        <h4>{{ member.name }}님, 환영합니다</h4>
                                         <p>자기소개를 입력해주세요</p>
                                         <div class="main-border-button">
-                                            <a id="dialog" href="member/update">회원정보수정</a>
-                                            <dialog>
+                                            <a id="dialog" href="" @click.prevent="showModal = true">회원정보수정</a>
+                                            <dialog :open="showModal" @close="showModal = false">
                                                 <h4>회원정보수정</h4>
                                                 <form name="form-member-update" onsubmit="return false;">
-                                                    <!-- 회원정보수정 모달 -->
+                                                    <input type="hidden" class="form-control" name="idx" :value="member.idx" >
+                                                    <label for="loginId">로그인ID</label>
+                                                    <input type="text" class="form-control" readonly id="loginId" name="loginId" :value="member.login_id" >
+                                                    <br>
+                                                    <label for="password">비밀번호</label>
+                                                    <input class="form-control" type="password" id="password" name="password" :value="member.password" readonly >
+                                                    <br>
+                                                    <label for="name">이름</label>
+                                                    <input class="form-control" type="text" id="name" name="name" :value="member.name" >
+                                                    <br>
+                                                    <label for="name">이메일</label>
+                                                    <input class="form-control" type="text" name="email" :value="member.email" >
+                                                    <br>
+                                                    <label for="name">전화번호</label>
+                                                    <input class="form-control" type="text" name="phone" :value="member.phone" >
+                                                    <br>
                                                 </form>
+                                                <div class="col-lg-12">
+                                                    <div class="main-button">
+                                                        <a @click="getUpdateMember" href="/mypage/home">저장하기</a>
+                                                    </div>
+
+                                                </div>
                                             </dialog>
                                         </div>
                                     </div>
@@ -36,6 +57,54 @@
                                     </ul>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="clips">
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div class="heading-section">
+                                                    <h4><em>최근</em> 주문내역</h4>
+                                                </div>
+                                                <template v-for="(order) in orderList" :key="order.idx">
+                                                    <h4>주문번호: <em>{{ order.idx }}</em></h4>
+                                                    <template v-for="(cartProduct) in order.cart.cartProductList"  :key="cartProduct.idx">
+                                                        <div class="item">
+                                                            <ul>
+                                                                <li>
+                                                                    <img v-bind:src="cartProduct.product.image_url" alt="" class="templatemo-item">
+                                                                    <a :href="`/product/detail?idx=${cartProduct.product.idx}`">
+                                                                        <h4>{{ cartProduct.product.name }}
+                                                                            <span style="margin-left: 10px; color: #1EDDFF;">{{ cartProduct.product.brand.name }}</span>
+                                                                        </h4>
+                                                                    </a>
+                                                                    <span>
+                                                                        <button class="review-insert">리뷰 작성</button>
+                                                                    </span>
+                                                                </li>
+                                                                <li>
+                                                                    <h4>카테고리</h4><span>{{ cartProduct.product.category.name }}</span>
+                                                                </li>
+                                                                <li>
+                                                                    <h4>상품가격</h4><span>{{ cartProduct.product.price }}</span>
+                                                                </li>
+                                                                <li>
+                                                                    <h4>주문일</h4><span>{{ order.regdate }}</span>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </template>
+                                                    <br>
+                                                </template>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="main-button">
+                                                <a href="#">TOP</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,11 +115,100 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { ref } from 'vue';
 export default {
+    data(){
+            return{
+                showModal:false
+            }
+        },
 
+    setup(){
+        //variable
+        const member = ref(null);
+        const orderList = ref(null);
+        const updateMember = ref(null);
+
+        //function
+        const getMember = async () => {
+            const result = await axios.get("/member/8");
+            member.value = result.data;
+            updateMember.value = {...result.data};
+
+            getOrderListByMember();
+        }
+        getMember();
+
+        const getOrderListByMember = async () => {
+            const memberIdx = member.value.idx;
+            const result = await axios.get("/order/member/" + memberIdx);
+            orderList.value = result.data;
+        }
+
+        const getUpdateMember = async() =>{
+            await axios.post("/member/save", updateMember.value);
+            console.log(updateMember.value);
+        }
+
+        //return
+        return {
+            member,
+            orderList,
+            getUpdateMember
+        }
+    }
 }
+
 </script>
 
 <style>
-  @import "@/assets/css/member-home.css";
+  *{
+    box-sizing: border-box !important;
+}
+
+.myh4 {
+    font-size: 25px;
+}
+
+dialog {
+    background-color: #27292a; 
+    background-size: cover;
+    min-height: 500px;
+    margin: auto;
+    border-radius: 23px;
+    padding: 80px 60px;
+    border-color: #1EDDFF;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    z-index: 1001;
+}
+
+dialog::backdrop {
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+label {
+    color: white;
+}
+
+
+.item ul li a h4 {
+	font-size: 17px;
+	margin-left: 15px;
+}
+
+.review-insert {
+	margin-top: 35px;
+	width: 110px;
+	height: 45px;
+	border-radius: 20px;
+	background-color: #1EDDFF;
+	color: white;
+	border: 1px solid #1EDDFF;
+}
+
+.review-insert:hover {
+    background-color: #fff;
+    color: #1EDDFF;
+}
 </style>
