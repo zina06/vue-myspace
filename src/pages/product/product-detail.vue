0c39808e317ec93c1a04c05840342297eda24c0a
jina <dhaud42@naver.com>
2023-03-27 11:05:40 +0900

    상세페이지 리뷰리스트

--- src/pages/product/product-detail.vue
+++ src/pages/product/product-detail.vue
@@ -14,7 +14,7 @@
 				<span style="font-size: 1.7em;"><b>{{ price }}원</b></span><br><br>
 				<span style="color: gray;">배송</span>&nbsp;&nbsp;&nbsp;
 				<span>배송상품정보 참고</span><br><hr>
-				<img alt="" src="" style="width: 30px; height: 30px;">
+				<img alt="" src="../../assets/images/brandimg.png" style="width: 30px; height: 30px;">
 				<span>{{ brand }}</span><br><br>
 				<span><b>주문금액 </b></span>
 				<span style="float: right;"><b>{{ price }}원</b></span><br><br>
@@ -38,38 +38,42 @@
   <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0" style="margin-left:500px; margin-top: 70px;">
   	 <span style="margin-left: 405px; font-size: 1.3em;"><b>구매만족도</b></span>
   	<div id="allTotal" style="background: #F3F3F3; border: 1px solid #F3F3F3; width: 100px;height: 100px; border-radius: 100%; font-size: 1.5em; margin-left: 410px; margin-top: 10px;">
-            
+           <span style="margin-left: 43px; margin-top: 25px; display: inline-block;"> {{ allTotal }}</span>
   	</div>
   	<br><br>
+	<div>
+	</div>
+
+	
   				
 									
 									
 									
+				  <template v-for="review in reviewList" :key="review.idx">
+									<div >
+  						<img src="../../assets/images/reviewprofile.png" style="width: 26px; height: 26px;">
+										<span style="display: inline-block; margin-bottom: 10px;">{{ review.member.name }}</span><br>
+										{{ review.score.total }} <span style="display: inline-block; margin-bottom: 10px;">
 									
-									
-  						<img src="" style="width: 26px; height: 26px;">
-										<span style="display: inline-block; margin-bottom: 10px;">이름</span><br>
-										총점 <input value="${rlist.total}" type="hidden" name="total"><span style="display: inline-block; margin-bottom: 10px;" id="total" name="total" >
-										점수
 										</span>
 										
 										
 										<br>
-										내구성 <c:forEach var="i" begin="0" end="5">
+										{{ review.score.durability }} <span v-for="review in reviewList" :key="review.idx">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-10" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-10"><rect x="0" y="0" width="24" height="24"></rect></clipPath></defs><use xlink:href="#star-path-10" fill="#DBDBDB"></use><use clip-path="url(#star-clip-10)" xlink:href="#star-path-10"></use></svg>
-										</c:forEach> 
+										</span> 
 										<c:forEach var="i" begin="5" end="5">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-19" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-19"><rect x="0" y="0" width="0" height="24"></rect></clipPath></defs><use xlink:href="#star-path-19" fill="#DBDBDB"></use><use clip-path="url(#star-clip-19)" xlink:href="#star-path-19"></use></svg>
 										</c:forEach>
 										&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;  
-										가격 <c:forEach var="i" begin="0" end="5">
+										{{ review.score.price }} <c:forEach var="i" begin="0" end="5">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-10" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-10"><rect x="0" y="0" width="24" height="24"></rect></clipPath></defs><use xlink:href="#star-path-10" fill="#DBDBDB"></use><use clip-path="url(#star-clip-10)" xlink:href="#star-path-10"></use></svg>
 										</c:forEach> 
 										<c:forEach var="i" begin="5" end="5">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-19" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-19"><rect x="0" y="0" width="0" height="24"></rect></clipPath></defs><use xlink:href="#star-path-19" fill="#DBDBDB"></use><use clip-path="url(#star-clip-19)" xlink:href="#star-path-19"></use></svg>
 										</c:forEach>
 										<br>
-										디자인 <c:forEach var="i" begin="0" end="5">
+										{{ review.score.design }} <c:forEach var="i" begin="0" end="5">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-10" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-10"><rect x="0" y="0" width="24" height="24"></rect></clipPath></defs><use xlink:href="#star-path-10" fill="#DBDBDB"></use><use clip-path="url(#star-clip-10)" xlink:href="#star-path-10"></use></svg>
 										</c:forEach> 
 										<c:forEach var="i" begin="5" end="5">
@@ -77,7 +81,7 @@
 										</c:forEach>
 										
 										&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; 
-										배송 <c:forEach var="i" begin="0" end="5">
+										{{ review.score.delivery }} <c:forEach var="i" begin="0" end="5">
                     						<svg fill="#35C5F0" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><defs><path id="star-path-10" d="M11.9996 19.7201L6.32294 22.1251C5.5626 22.4472 5.005 22.0311 5.0755 21.2188L5.60855 15.0767L1.5671 10.421C1.02579 9.79745 1.24924 9.13855 2.04358 8.95458L8.04973 7.56354L11.2287 2.28121C11.6545 1.57369 12.3502 1.5826 12.7706 2.28121L15.9496 7.56354L21.9557 8.95458C22.7602 9.1409 22.9667 9.8053 22.4322 10.421L18.3907 15.0767L18.9238 21.2188C18.9952 22.0414 18.4271 22.4432 17.6764 22.1251L11.9996 19.7201Z"></path><clipPath id="star-clip-10"><rect x="0" y="0" width="24" height="24"></rect></clipPath></defs><use xlink:href="#star-path-10" fill="#DBDBDB"></use><use clip-path="url(#star-clip-10)" xlink:href="#star-path-10"></use></svg>
 										</c:forEach> 
 										<c:forEach var="i" begin="5" end="5">
@@ -89,12 +93,13 @@
 										<br><br>
 										<div>
 										<div style="background: lightgray; border: 1px solid #F3F3F3; width: 3px;height: 25px;"></div>
-										<b style="position: relative; bottom: 25px; left: 10px;">상품이름</b><br>
+										<b style="position: relative; bottom: 25px; left: 10px;">{{productName}}</b><br>
 										</div>
 										
-										 리뷰내용
+										 {{review.content}}
 										 <br> <hr style="width: 800px;"><br>
-								
+										</div>
+									</template>
   
   </div>
   <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">...</div>
@@ -117,6 +122,8 @@ export default {
 		const price=ref('');
 		const image_url=ref('');
 		const idx=route.params.idx;
+		const reviewList=ref(null);
+		const allTotal=ref('');
 		
 		const getProduct = async() =>{
 			//loading.value=true;
@@ -130,13 +137,32 @@ export default {
 				productName.value=product.data.name
 				price.value=product.data.price
 				image_url.value=product.data.image_url
-				
+				allTotal.value=product.data.allTotal
 				
 			})
+
+
 		};
-			getProduct();
+		getProduct();
+
+		const getReviewList=async()=>{
+			
+			const res = await axios.get(`/review/product/${idx}`).then((review)=>{
+				console.log(review.data);
+				console.log(review.data[0].content);
+				console.log(review.data[0].score.durability);
+				reviewList.value=review.data;
+
+				
+			})
+		}
+			
+		getReviewList();
+
 	return{
 		getProduct,
+		reviewList,
+		getReviewList,
 		categoryName,
 		brand,
 		productName,
@@ -144,7 +170,8 @@ export default {
 		image_url,
 		idx,
 		route,
-		router
+		router,
+		allTotal
 	};	
 			
 		
